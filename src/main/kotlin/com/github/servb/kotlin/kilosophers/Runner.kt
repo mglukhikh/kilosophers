03974f43545a50f9f806891e394cc5c124d6cc06
SerVB <servbul@yandex.ru>
2018-05-18 01:17:57 +0300

    Add statistics

--- src/main/kotlin/com/github/servb/kotlin/kilosophers/Runner.kt
+++ src/main/kotlin/com/github/servb/kotlin/kilosophers/Runner.kt
@@ -1,16 +1,19 @@
 package com.github.servb.kotlin.kilosophers
 
 fun main(args: Array<String>) {
-    val table = Table(5)
-    var tick = 0
+    val n = 5
+    val eatStat = IntArray(n, { 0 })
+    val table = Table(n)
 
+    var tick = 0
     while (true) {
         for (i in table.kilosophers.indices) {
             val nowKilosopher = table.kilosophers[i]
             nowKilosopher.act(100)
 
             if (nowKilosopher.state == MyKilosopher.State.EATS) {
-                println("$tick: $i eats!")
+                ++eatStat[i]
+                println("$tick: $i eats! Stats: ${eatStat.joinToString(" ")}.")
             }
         }
 
