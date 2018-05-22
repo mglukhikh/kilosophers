b587f8f861627b3c6da48dd7ed099cc30ddb6a23
SerVB <servbul@yandex.ru>
2018-05-22 12:08:56 +0300

    Update to Kotlin style

--- src/main/kotlin/com/github/servb/kotlin/kilosophers/Runner.kt
+++ src/main/kotlin/com/github/servb/kotlin/kilosophers/Runner.kt
@@ -2,13 +2,12 @@ package com.github.servb.kotlin.kilosophers
 
 fun main(args: Array<String>) {
     val n = 5
-    val eatStat = IntArray(n, { 0 })
+    val eatStat = IntArray(n) { 0 }
     var table = Table(n)
 
     var tick = 0
     while (true) {
-        for (i in table.kilosophers.indices) {
-            val nowKilosopher = table.kilosophers[i]
+        for ((i, nowKilosopher) in table.kilosophers.withIndex()) {
             nowKilosopher.act(100)
 
             if (nowKilosopher.state == MyKilosopher.State.EATS) {
--- src/main/kotlin/com/github/servb/kotlin/kilosophers/Table.kt
+++ src/main/kotlin/com/github/servb/kotlin/kilosophers/Table.kt
@@ -5,10 +5,10 @@ import ru.spbstu.kilosophers.atomic.AtomicFork
 class Table(val n: Int) {
     val waiter = WaiterNearTable(this)
 
-    val forks = Array(n, { AtomicFork() })
+    val forks = Array(n) { AtomicFork() }
     val kilosophers = Array(n, { MyKilosopher(forks[it % n], forks[(it + 1) % n], waiter) })
 
-    val freeForks get() = forks.filter { it.isFree() }.size
+    val freeForks get() = forks.count { it.isFree() }
 
     val isLocked get() = kilosophers.all { it.state == MyKilosopher.State.WAITS_RIGHT }
 }
